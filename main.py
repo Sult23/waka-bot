a2234f90e2c0d815779d24bf205db1b4674e21aa
Sult23 <92369763+Sult23@users.noreply.github.com>
2025-07-06 20:44:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Мы продаём электронные сигареты Waka с доставкой по городу Тараз.
+        "Мы продаём электронные сигареты Waka с доставкой по городу Тараз."
 "
         "Оплата Kaspi или наличными при получении."
     )
